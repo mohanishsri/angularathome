66834f8dc2986575dd7ae0e1f0ae3aae37ef5c4d
mohanishsri <mohanish.sri@gmail.com>
2018-01-23 22:42:09 +0530

    My angular try at home

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { NgModule } from '@angular/core';
+
+
+import { AppComponent } from './app.component';
+import { ContactFormComponent } from './contact-form/contact-form.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ContactFormComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
